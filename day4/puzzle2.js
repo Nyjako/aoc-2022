9a9756388f1c5e055004b8cb068a30bb0b06b511
Kacper Tucholski <31698318+Nyjako@users.noreply.github.com>
2022-12-11 16:13:58 +0100

    day4 puzzle2

---
+++ day4/puzzle2.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+function overlap(left, right) {
+	const [left1, left2]   = left.split('-');
+	const [right1, right2] = right.split('-');
+	
+	return (
+		parseInt(left1) <= parseInt(right1) && parseInt(left2) >= parseInt(right1) || 
+		parseInt(left1) <= parseInt(right2) && parseInt(left2) >= parseInt(right2) ||
+		parseInt(right1) <= parseInt(left1) && parseInt(right2) >= parseInt(left1) || 
+		parseInt(right1) <= parseInt(left2) && parseInt(right2) >= parseInt(left2)
+	);
+}
+
+const lines = fs.readFileSync('puzzle.txt').toString().split('\n');
+let counter = 0;
+
+for (line of lines) {
+	if (line == '') continue;
+
+	const [left, right] = line.split(',');
+	let t = overlap(left, right);
+
+	if (t) counter++;
+	console.log(t);
+}
+
+console.log(counter);
